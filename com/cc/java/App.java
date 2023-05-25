107401ae0376831c73883306a976534341d42b35
sven-pke <sven-perschke@gmx.de>
2023-05-25 08:06:12 +0200

    läuft

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,15 +3,19 @@ package com.cc.java;
 public class App {
 
 	public static void main(String[] args) {
-	
-	
+        Mitarbeiter mitarbeiter1 = new Mitarbeiter("Dampf", "Hans", "Entwickler", 2021);
+        Mitarbeiter mitarbeiter2 = new Mitarbeiter("Ming", "Phil", "Designer", 2022);
+        Mitarbeiter mitarbeiter3 = new Mitarbeiter("Sauer", "Siggi", "Hausmeister", 2023);
 
-	}
+        output(mitarbeiter1.getInfo("vorname", "name")); 
+        output(mitarbeiter2.getInfo("vorname", "name", "funktion")); 
+        output(mitarbeiter3.getInfo( "vorname", "name", "funktion", "eintrittsjahr")); 
+    }
 
-	
 
 	private static void output(String outStr) {
 		System.out.println(outStr);
 	}
 
-};
+
+}
---
+++ com/cc/java/Mitarbeiter.java
@@ -0,0 +1,39 @@
+package com.cc.java;
+
+public class Mitarbeiter {
+    private String familyName;
+    private String firstName;
+    private String role;
+    private int yearOfEntry;
+
+    public Mitarbeiter(String familyName, String firstName, String role, int yearOfEntry) {
+        this.familyName = familyName;
+        this.firstName = firstName;
+        this.role = role;
+        this.yearOfEntry = yearOfEntry;
+    }
+
+    public String getInfo(String... fields) {
+        StringBuilder sb = new StringBuilder();
+        for (String field : fields) {
+            switch (field.toLowerCase()) {
+                case "name":
+                    sb.append(this.familyName).append(" ");
+                    break;
+                case "vorname":
+                    sb.append(this.firstName).append(" ");
+                    break;
+                case "funktion":
+                    sb.append(this.role).append(" ");
+                    break;
+                case "eintrittsjahr":
+                    sb.append(this.yearOfEntry).append(" ");
+                    break;
+                default:
+                    sb.append("Das passt hier nicht!").append(" ");
+                    break;
+            }
+        }
+        return sb.toString().trim();
+    }
+}
